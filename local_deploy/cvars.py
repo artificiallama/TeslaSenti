f089c5bac1f42d458e9dbe24fe8f0e5a1f1c4f9e
artificiallama <email_id>
2020-12-22 14:34:14 -0500

    value of time_horizon is defined here.

--- local_deploy/cvars.py
+++ local_deploy/cvars.py
@@ -8,3 +8,6 @@ cols_display = ['id', 'date', 'tweet', 'senti', 'screen_name', 'tidy_tweet']
 # any tweets with more than cash_thresh cashtags are removed.
 cash_thresh = 10
 nsecs=20
+
+# sentiment index for how many minutes before current time should be shown in the graph ?
+time_horizon=60
