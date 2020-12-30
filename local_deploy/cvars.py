2c6fa4d3174ec2f24f74be628e15b5a18c2c5617
artificiallama <email_id>
2020-12-30 11:42:33 -0500

    Increase horizon time to 24 and 2 hours.

--- local_deploy/cvars.py
+++ local_deploy/cvars.py
@@ -13,8 +13,8 @@ cash_thresh = 10
 nsecs=20
 
 # sentiment index for how many minutes before current time should be shown in the graph ?
-time_horizon1=600
-time_horizon2=60
+time_horizon1=24*60
+time_horizon2=120
 
 # Tick interval on xaxis of the plotly graph. The unit of tick_step is minutes. The unit expected by plotly
 # is milliseconds.
