Fixed a bug in the generation of time series of average sentiment index.
The short time series goes back 1 hour. The long timeseries goes back 24 hours. the short time series has binning size of 5 minutes while that for long timeseries in 1 hour. This bug affected both the time series.

As time advances the graph of avergare sentiment index recedes as the latest average is appended to the graph. The averages over the earlier bins should not change. For example if the latest average corresponds to 08:00, the average corresponding to the first bin 07.00 (which averages between 07:00 and 07:05) should not change. However as current time advances beyond 08:00 the average over the first bin changes. This is because the time_horizon remains fixed for a given time series. If the current time is 08:00:00 the first binning interval is from 07:00:00-07:05:00. If current time is 08:02 the first binning interval starts from
07:02. Thus the fist bin average loses tweets for the first 2 minutes. When the current time advances to 08:04:00, the first bin averages tweets only over the 1 minute from 07:04 to 07:05.

This bug is fixed by using the floor() function. The starting point of the timeseries is rounded of to a multiple of 60 minutes and 5 minutes for the long and short time series respectively.

There is some scope for optimization in the calculation of averages over bins for the past time. At every refresh one needs to calculate only the average over the latest bin. The older bin averages have been calculated during the earlier refreshes. If these are persisted across refreshes there is no need to calcualte them saving some time in serving the request.

local_deploy/app.py
@@ -68,8 +68,8 @@ def home():
     for kk in np.arange(nrows-4,nrows):  
         #print('\n\t** kk = ',kk, df.iloc[kk]['id'])
         #print('\t** screenname = ',df.iloc[kk]['screen_name'])
-        print('\t** tweet = ',df.iloc[kk]['tweet'])    
-        print('\t** senti = ',df.iloc[kk]['senti'],df.iloc[kk]['wt_senti'])   
+        #print('\t** tweet = ',df.iloc[kk]['tweet'])    
+        #print('\t** senti = ',df.iloc[kk]['senti'],df.iloc[kk]['wt_senti'])   
         htmlcode = get_tweet_embed_html(df.iloc[kk]['screen_name'],str(df.iloc[kk]['id']))
         tweet_embed.append(htmlcode)
 
@@ -77,8 +77,10 @@ def home():
     fnms='tweets_latest_subset_2020-07-13_17-14-17_to_2020-07-13_17-19-17.csv'
 
     timenow = tm.current_time()
-    tminus = tm.lag_time(timenow,cvars.time_horizon1)
-
+    tminus = pd.Series(tm.lag_time(timenow,cvars.time_horizon1)).dt.floor('60min')[0]
+    print('\n\ttimenow = ',timenow)
+    print('\ttminus  = ',tminus)
+	
     cond =  df['date'].apply(lambda x : tm.isin_window(tminus,timenow,tm.dstr_obj(x)))
     df.drop(index=df[~cond].index,inplace=True)
 
@@ -116,8 +118,12 @@ def give_graph(dfin,tnow):
     fig.add_trace(longser,row=1,col=1)
     fig.add_trace(longavg2,row=1,col=1)
     fig.add_trace(longavg,row=1,col=1)
+
+    tminus = pd.Series(tm.lag_time(tnow,cvars.time_horizon2)).dt.floor('5min')[0]
+    print('\n\ttimenow = ',tnow)
+    print('\ttminus  = ',tm.lag_time(tnow,cvars.time_horizon2))
+    print('\ttminus  = ',tminus)
 	
-    tminus = tm.lag_time(tnow,cvars.time_horizon2)
     cond =  dfin['dtobj'].apply(lambda x : tm.isin_window(tminus,tnow,x))
     dfin.drop(index=dfin[~cond].index,inplace=True)	
 