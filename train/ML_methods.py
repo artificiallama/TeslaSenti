add6ea657673ae48b13b20a68594acb6dd41360d
artificiallama <email_id>
2021-03-07 11:25:00 -0500

    Add docstring

--- train/ML_methods.py
+++ train/ML_methods.py
@@ -1,11 +1,30 @@
 from sklearn.model_selection import GridSearchCV, StratifiedKFold
 import pandas as pd
 
-# Figure out best parameters amongst check_params for model so that
-# refit_score is maximized.
-
 
 def grid_search_wrapper(model, x_train, y_train, check_params, refit_score):
+    """Identify best parameters amongst check_params for fitting the model.
+
+    The model is fitted using every combination of parameter values found
+    in check_params with crossvalidation. The best parameter values maximise
+    the refit_score for the fitted model. The best model is returned along
+    scores for each fitted model. This includes the mean and standard deviation
+    of train and test scores.
+
+    :param model: The model to be fitted
+    :type  model: Instance of sklearn base class
+    :param x_train: Features for fitting the model
+    :type  x_train: Scipy 2d float array
+    :param y_train: Targets for fitting the model
+    :type  y_train: Pandas series
+    :param check_params: Names and values of parameters to be searched
+    :type  check_params: Dictionary
+    :param refit_score: The score to be maximized
+    :type  refit_score: str
+    :return: The best model and  scores of all models are returned
+    :rtype:  (sklearn class instance, pandas dataframe)
+
+    """
 
     clf = GridSearchCV(model, param_grid=check_params,
                        refit=refit_score,
