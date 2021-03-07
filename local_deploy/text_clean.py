1efe22781284397adb0eac42c58d91238b4dfbcd
artificiallama <email_id>
2021-03-07 15:16:50 -0500

    Add docstrings for better documentation.

--- local_deploy/text_clean.py
+++ local_deploy/text_clean.py
@@ -1,3 +1,13 @@
+"""Functions to clean and normalize text.
+
+   Remove irrelevant text. Eg. For tweets mentions,hashtags
+   cashtags, embedded graphs/smileys are noise and should be
+   removed before performing feature extraction using BOW
+   or tf-idf,etc. These functions can be used for text including
+   newspaper headlines which usually do not contain mentions,
+   cashtags etc.
+"""
+
 import re
 from nltk.stem.porter import PorterStemmer
 from nltk.stem import WordNetLemmatizer
@@ -15,50 +25,68 @@ stop_words = nltk.corpus.stopwords.words('english')
 # these words should not be part of stop_words since these are relevant
 # for stocks.
 remove_words = ['above', 'below', 'up',  'down', 'off',  'over', 'under']
-more_stop_words = ['would','get','also']
+more_stop_words = ['would', 'get', 'also']
 
 
 for w in remove_words:
     stop_words.remove(w)
 
 for w in more_stop_words:
- stop_words.append(w)	
+    stop_words.append(w)
 
 p.set_options(p.OPT.EMOJI, p.OPT.SMILEY,  p.OPT.URL)
 
 
-# Important for tweets/posts
 def clean_emoji_url(x):
+    """Remove emoji, smileys and urls."""
+
     return p.clean(x)
 
 
 def remove_mention(input_txt):
+    """Remove @tokens."""
+
     return re.sub("@[\w]*", '', input_txt)
 
 
 def remove_hashtag(input_txt):
+    """Remove #tokens."""
+
     return re.sub("#[\w]*", '', input_txt)
 
 
 def remove_cashtag(input_txt):
+    """Remove $tokens."""
+
     return re.sub("[$][\w]*", '', input_txt)
 
 
-# Count cashtags which are not ($TSLA, $TSLAQ)
 def count_cashtags(input_txt):
+    """Count cashtags which are not ($TSLA, $TSLAQ) """
+
     ff = re.findall('[$][a-zA-Z]+', input_txt)
+
     return len([tk for tk in ff if tk.lower() not in ['$tsla', '$tslaq']])
 
 
 def replace_chars(input_txt):
+    """Replace &amp and embedded pictures/graphs."""
+
     input_txt = input_txt.replace("&amp", " and ")
-# Replace embedded pictures/graphs in tweets
+
     input_txt = input_txt.replace('\U0001f4c8', " ")
+
     return input_txt
 
 
 def normalize_doc(txt):
-    txt = re.sub('[^a-zA-Z]', ' ', txt)  # Remove everything except text.
+    """Remove everything except text.
+
+    Stop words are removed.
+    All punctuation is removed.
+    """
+
+    txt = re.sub('[^a-zA-Z]', ' ', txt)
     txt = txt.lower()
     tok_list = word_tokenize(txt)
     filtered_tok_list = [token for token in tok_list if token not in
@@ -68,4 +96,6 @@ def normalize_doc(txt):
 
 
 def count_toks(txt):
+    """Return number of words."""
+
     return len(word_tokenize(txt))
